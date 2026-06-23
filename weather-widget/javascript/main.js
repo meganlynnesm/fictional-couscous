e435c914d1a871713e48defdb491fb9ee8ecd146
meganlynnesm <meganmackin0@gmail.com>
2026-06-22 20:04:43 -0400

    stage 3.6
    Data and APIs # replace Create index.html
    Interactive Web submission # replace Description
    (I've been constantly updating this, so progress is minimal in the screenshot)

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,2 +1,7 @@
 console.log("This works");
 const API_KEY = config.WEATHER_API_KEY;
+function getWeatherData() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=10011&APPID=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => console.log(data));
+    }
